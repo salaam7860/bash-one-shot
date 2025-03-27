7d6cc46dfdc603473f3f986699f89d5d73ac05dd
saad zaheer <salaam7860@gmail.com>
2025-03-27 20:51:32 +0500

    add git

---
+++ backup_scirpt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Author: saad
+# Date: 18 sep 2024
+# last modified: 18 sep 2024
+# Description: creates a backup of ~/bash_project of all files in the home directory 
+#
+# Usage: backup_script
